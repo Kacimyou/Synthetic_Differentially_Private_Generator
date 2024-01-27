Fix shuffle in generate synthetic data + found formula for smoothed

Synthetic-data-generator/histogram.py
@@ -4,7 +4,7 @@ import matplotlib.pyplot as plt
 import scipy.stats as stats
 
 #%%
-def modified_histogram_estimator(X, h=0.1, adaptative = True, privacy = None, delta = 0.1):
+def modified_histogram_estimator(X, h=0.1, adaptative = True):
     """
     Computes the normalized histogram estimator for multidimensional data with a specified number of bins per axis.
 
@@ -72,7 +72,6 @@ def modified_histogram_estimator(X, h=0.1, adaptative = True, privacy = None, de
     rescaling_factors = np.array([np.min(X, axis=0), np.max(X, axis=0)])
     
 
-  
     return fbm, rescaling_factors
 
 
@@ -114,6 +113,8 @@ def generate_data_from_fbm(fbm_estimator, m, rescaling_factor = [1,0], shuffle =
 
         #TODO FIX noise problem check with QQ PLOT
         print([(idx + noise) for idx, noise in zip(multi_dim_indices, random_noise)])
+        
+        
         # Use the list of random numbers within the list comprehension
         synthetic_data = np.column_stack(
             [rescaling_factor[0] + ((idx + noise) * binwidth * (rescaling_factor[1] - rescaling_factor[0])) for idx, noise in zip(multi_dim_indices, random_noise)]
@@ -138,7 +139,7 @@ def main(args_dict):
     # TODO : FIX data type problem with random.rand vs random.normal (chiant)
 
     # Perform histogram estimation
-    fbm_estimator, rescaling_factors = modified_histogram_estimator(X, adaptative=adaptative, privacy= "smoothing")
+    fbm_estimator, rescaling_factors = modified_histogram_estimator(X, adaptative=adaptative)
 
     # Generate synthetic data
     synthetic_data = generate_data_from_fbm(fbm_estimator, m, rescaling_factors, shuffle= True)
@@ -202,3 +203,6 @@ plt.show()
 print(synthetic_data_sorted[:10])
 
 # %%
+
+
+modified_histogram_estimator

test.py
@@ -1,14 +1,85 @@
 import numpy as np 
+import matplotlib as plt
 
 
-x = np.random.normal(size= 1000)
-
-print(x[:10])
+prob = np.array([0.006, 0.05,  0.198, 0.36,  0.272, 0.108, 0.004, 0.002])
 
 
+print(np.sum(prob))
+
+delta = 0.1
+m = len(prob)
+
+
+smoothed = (1 - delta)*prob + delta/m
+
+#print(smoothed, np.sum(smoothed))
 
 
 
+def modified_histogram_estimator(X, h=0.1, adaptative = True, delta = 0.1):
+    
+    # Calculate the dimensions of the data
+    n, d = X.shape
+
+    """if np.min(X, axis=0) < 0 or np.max(X, axis=0) > 1:
+        # Scale the data to [0, 1] range
+        X_scaled = (X - np.min(X, axis=0)) / (np.max(X, axis=0) - np.min(X, axis=0))
+    
+    #elif np.min(X, axis=0) >= 0 and np.max(X, axis=0) <= 1:
+        
+        X_scaled = X
+    """
+    
+    X_scaled = (X - np.min(X, axis=0)) / (np.max(X, axis=0) - np.min(X, axis=0))
+    
+    if adaptative == True:
+        # Calculate the binwidth based on the given parameters
+        h = n**(-1 / (2 + d))
+    
+    assert h<1 and h>0 , "Error: h must be between 0 and 1"
+    
+    
+    # Check if 1/h is an integer, and convert if necessary
+    m_inverse = 1 / h
+    if not m_inverse.is_integer():
+        m_per_axis = int(np.round(m_inverse))
+        print(f"Warning: 1/h is not an integer. Converting to the closest integer: {m_per_axis}")
+    else:
+        m_per_axis = int(m_inverse)
+    
+
+    # Initialize the histogram estimator
+    fbm_shape = (m_per_axis,) * d
+    fbm = np.zeros(fbm_shape)
+
+    # Iterate through each row as a separate data point
+    for i in range(n):
+        # Determine the bin index for the current data point in each dimension
+        bin_indices = tuple(int(np.floor(X_scaled[i, j] / h)) for j in range(d))
+        
+        # Update the histogram estimator
+        fbm[bin_indices] += 1
 
 
+    # Normalize the histogram estimator
+    fbm /= n
+    #TODO CONTINUE the implementation of privacy options
+    print(np.sum(fbm),fbm, np.full(fbm_shape, delta))
+    
+    fbm = (1-delta)*fbm + delta/(m_per_axis**d)
 
+    print(fbm, np.sum(fbm))
+    # Check if the sum of elements is equal to 1
+    
+    assert np.isclose(np.sum(fbm), 1.0), "Error: Sum of histogram elements is not equal to 1."
+    
+    
+    rescaling_factors = np.array([np.min(X, axis=0), np.max(X, axis=0)])
+
+
+    return fbm, rescaling_factors
+
+X = np.random.normal(loc=0, scale=1, size=(1000, 2))
+
+modified_histogram_estimator(X)