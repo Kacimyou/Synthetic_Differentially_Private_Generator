06a6232adbe0e23a3ac62763bfe8cc3a2a8166fa
Kacim Younsi <104354290+Kacimyou@users.noreply.github.com>
2024-04-05 15:37:17 +0200

    Fix error bars

--- Synthetic-data-generator/src/plot_linear_reg.py
+++ Synthetic-data-generator/src/plot_linear_reg.py
@@ -6,7 +6,7 @@ from super_regular_noise import generate_super_regular_noise_data
 from linear_stat_fit import generate_auto_linear_stat_fit_data
 from sklearn.linear_model import LinearRegression
 from sklearn.metrics import r2_score
-
+import scipy.stats
 
 # %%
 
@@ -17,7 +17,7 @@ def generate_data(X, size, epsilon, method):
     elif method == "smooth":
         return generate_smooth_data(X, size, epsilon)
     elif method == "linear_stat_fit":
-        return generate_auto_linear_stat_fit_data(X, size, epsilon)
+        return generate_auto_linear_stat_fit_data(X, size, epsilon, method="grid_noid")
     elif method == "super_regular_noise":
         return generate_super_regular_noise_data(X, size, epsilon)
     else:
@@ -30,14 +30,14 @@ def run_experiment(N, epsilon, methods, num_trials=10):
 
     for method in methods:
         mean_r2_scores = []
-        variances = []
+        stds = []
         for size in sizes:
             r2_scores = []
             for _ in range(num_trials):
-                X = np.random.multivariate_normal(
+                data = np.random.multivariate_normal(
                     mean=[0, 1], cov=[[1, 0.9], [0.9, 1]], size=size
                 )
-                private_data = generate_data(X, size, epsilon, method)
+                private_data = generate_data(data, size, epsilon, method)
 
                 x = private_data[:, 0].reshape(-1, 1)
                 y = private_data[:, 1]
@@ -53,17 +53,22 @@ def run_experiment(N, epsilon, methods, num_trials=10):
 
             # Calculate mean R2 score and variance
             mean_r2 = np.mean(r2_scores)
-            variance = np.var(r2_scores)
+            std = np.std(r2_scores)
             mean_r2_scores.append(mean_r2)
-            variances.append(variance)
+            stds.append(std)
 
+        stds = np.array(stds) * (scipy.stats.norm.ppf(0.975) / np.sqrt(num_trials))
         # Plot mean R2 scores with variance for each method
         plt.errorbar(
             sizes,
             mean_r2_scores,
-            yerr=variances,
+            yerr=stds,
             label=f"Method: {method}, Epsilon={epsilon}",
             fmt="-o",
+            linestyle="-",
+            capsize=3,  # Length of the error bar caps
+            capthick=0.5,  # Thickness of the error bar caps
+            barsabove=True,  # Error bars are drawn above the markers
         )
 
     plt.xlabel("Size of Initial Database")
@@ -76,7 +81,7 @@ def run_experiment(N, epsilon, methods, num_trials=10):
 
 # Example usage:
 # methods = ["super_regular_noise","perturbated"]
-methods = ["linear_stat_fit"]
-run_experiment(2000, 2, methods, num_trials=1)
+methods = ["perturbated"]
+run_experiment(1000, 0.2, methods, num_trials=100)
 
 # %%
