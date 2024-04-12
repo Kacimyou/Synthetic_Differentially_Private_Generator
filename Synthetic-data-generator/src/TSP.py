ece3f890ea29c64673bb3c4bf75e014d558417d2
Kacim Younsi <104354290+Kacimyou@users.noreply.github.com>
2024-04-12 16:30:25 +0200

    Add plot functions for illustration

--- Synthetic-data-generator/src/TSP.py
+++ Synthetic-data-generator/src/TSP.py
@@ -48,7 +48,7 @@ def tsp(points, norm=2):
             G.add_edge(i, j, weight=weight)
 
     # TODO Clean solving algorithm, precising what method is used.
-    # tsp_path = nx.approximation.traveling_salesman_problem(G, cycle=False)
+    # tsp_path = nx.approximation.traveling_salesman_problem(G)
     tsp_path = nx.approximation.greedy_tsp(G)
     tsp_path.pop()
     tsp_path = [points[i] for i in tsp_path]
@@ -195,39 +195,157 @@ def from_1D_to_multi(one_d_histogram, reverse_dict, shape):
 
 # %%
 
-points = generate_grid_points(11, 2)
 
-# %%
-tsp_path = tsp(points, norm=2)
+m = 4
+points = generate_grid_points(m, 2)
+# Set figure size
+plt.gcf().set_size_inches(12, 6)
 
-plt.scatter(*zip(*points))
-for i in range(len(tsp_path) - 1):
-    plt.arrow(
-        tsp_path[i][0],
-        tsp_path[i][1],
-        tsp_path[i + 1][0] - tsp_path[i][0],
-        tsp_path[i + 1][1] - tsp_path[i][1],
-        color="red",
-        head_width=0.03,
-        head_length=0.05,
-    )
-plt.xlabel("X")
-plt.ylabel("Y")
-plt.title("Traveling Salesman Problem")
-plt.legend()
-plt.grid(True)
+
+def plot_mapping(points, square=False, arrow=True):
+    # Calculate TSP path
+    tsp_path = tsp(points, norm=2)
+
+    # Assign colors to original grid points with a gradient based on index
+    colors = plt.cm.viridis(np.linspace(0, 1, len(points)))
+
+    # Plot for TSP path
+    plt.subplot(1, 2, 1)
+
+    # Determine the size of the square
+    square_size = 1 / m - 10 ** (-3)
+    if square:
+        # Plot squares around each point with the same color as the point
+        for i, point in enumerate(points):
+            x, y = point
+            square = plt.Rectangle(
+                (x - square_size / 2, y - square_size / 2),
+                square_size,
+                square_size,
+                color=colors[i],
+                alpha=0.30,
+            )
+            plt.gca().add_patch(square)
+        for i in range(len(points)):
+            plt.axvline(points[i][0] - square_size / 2, color="grey", linestyle="--")
+            plt.axvline(points[i][0] + square_size / 2, color="grey", linestyle="--")
+            plt.axhline(points[i][1] - square_size / 2, color="grey", linestyle="--")
+            plt.axhline(points[i][1] + square_size / 2, color="grey", linestyle="--")
+
+        for i, point in enumerate(tsp_path):
+            plt.annotate(
+                r"$\mathcal{I}_{%s}$" % (i + 1),
+                (point[0], point[1]),
+                textcoords="offset points",
+                xytext=(-15, -10),
+                ha="center",
+                fontsize=8,
+            )
+
+    if arrow:
+        for i in range(len(tsp_path) - 1):
+
+            dx = tsp_path[i + 1][0] - tsp_path[i][0]
+            dy = tsp_path[i + 1][1] - tsp_path[i][1]
+            distance = np.sqrt(dx**2 + dy**2)
+            plt.arrow(
+                tsp_path[i][0],
+                tsp_path[i][1],
+                dx,
+                dy,
+                color="grey",
+                head_width=0.02,
+                head_length=0.03,
+                width=0.0005,
+            )
+            plt.text(
+                tsp_path[i][0] + dx / 2,
+                tsp_path[i][1] + dy / 2,
+                r"$\delta_{%s} = %.2f$" % (i + 1, distance),
+                fontsize=8,
+            )
+
+    plt.xlabel("X")
+    plt.ylabel("Y")
+    plt.title("Traveling Salesman Problem")
+    plt.grid(False)
+
+    # Annotate the points based on tsp_path
+    for i, point in enumerate(tsp_path):
+        plt.annotate(
+            r"$\omega_{%s}$" % (i + 1),
+            (point[0], point[1]),
+            textcoords="offset points",
+            xytext=(10, 3),
+            ha="center",
+            fontsize=8,
+        )
+        plt.scatter(tsp_path[i][0], tsp_path[i][1], marker="o", color=colors[i])
+
+    # Compute cumulative distances
+    cumulative_distances = [0]
+    for i in range(1, len(tsp_path)):
+        cumulative_distances.append(
+            cumulative_distances[-1]
+            + np.linalg.norm(np.array(tsp_path[i]) - np.array(tsp_path[i - 1]))
+        )
+
+    plt.subplot(1, 2, 2)
+
+    plt.title("Mapping TSP Path to Line")
+    plt.gca().get_yaxis().set_visible(False)  # Hide the y-axis
+    plt.axhline(0, color="black", linestyle="--")  # Plot horizontal line at y = 0
+
+    plt.scatter(
+        cumulative_distances, [0] * len(tsp_path), marker="o", color=colors
+    )  # Plotting the points with colors
+    path_length = len(tsp_path)
+
+    plt.title("Mapping TSP Path to Line")
+    for i, point in enumerate(tsp_path):
+        plt.annotate(
+            r"$\omega_{%s}$" % (i + 1),
+            (cumulative_distances[i], 0),
+            textcoords="offset points",
+            xytext=(0, 10),
+            ha="center",
+            fontsize=8,
+        )
+
+    # Annotate arrows with distance
+    for i in range(len(tsp_path) - 1):
+        dx = cumulative_distances[i + 1] - cumulative_distances[i]
+        dy = 0
+        distance = np.sqrt(dx**2 + dy**2)
+
+        plt.text(
+            cumulative_distances[i] + dx / 5,
+            0.005 * (-1) ** i,
+            r"$\delta_{%s} = %.2f$" % (i + 1, distance),
+            fontsize=8,
+        )
+
+    plt.xlabel("Path Length")
+
+    plt.tight_layout()
+    plt.show()
+
+
+uniform = np.random.uniform(0, 1, size=(10, 2))
+plot_mapping(points, square=False)
+
 # %%
 
 ##Test that the application of both mapping gives back the original histogram
-n = 10000
-d = 3
+# n = 10000
+# d = 3
 
-X = np.random.normal(loc=0, scale=1, size=(n, d))
+# X = np.random.normal(loc=0, scale=1, size=(n, d))
 
-hist, rescale = histogram_estimator(X)
+# hist, rescale = histogram_estimator(X)
 
 
-one_dim_array, reverse_dict, shape, omega = from_multi_to_1D(hist)
+# one_dim_array, reverse_dict, shape, omega = from_multi_to_1D(hist)
 
-multi_dim = from_1D_to_multi(one_dim_array, reverse_dict, shape)
-print(multi_dim == hist)
+# multi_dim = from_1D_to_multi(one_dim_array, reverse_dict, shape)
+# print(multi_dim == hist)
