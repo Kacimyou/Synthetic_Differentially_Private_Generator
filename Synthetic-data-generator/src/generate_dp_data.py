cc6cc1b945f70216befc6830d14d49f2e58607b7
Kacim Younsi <104354290+Kacimyou@users.noreply.github.com>
2024-04-22 14:27:28 +0100

    Create generate_dp_data.py

---
+++ Synthetic-data-generator/src/generate_dp_data.py
@@ -0,0 +1,31 @@
+from histogram_DP_mechanism import (
+    generate_smooth_data,
+    generate_perturbated_data,
+)
+from super_regular_noise import generate_super_regular_noise_data
+from linear_stat_fit import generate_auto_linear_stat_fit_data
+
+
+def generate_data(X, size, epsilon, method, shuffle=True, rescaling=True):
+    if method == "perturbated":
+        return generate_perturbated_data(
+            X, size, epsilon, shuffle=shuffle, rescaling=rescaling
+        )
+    elif method == "smooth":
+        return generate_smooth_data(
+            X, size, epsilon, shuffle=shuffle, automatic=True, rescaling=rescaling
+        )
+    elif method == "linear_stat_fit_grid":
+        return generate_auto_linear_stat_fit_data(
+            X, size, epsilon, method="classic", shuffle=shuffle, rescaling=rescaling
+        )
+    elif method == "linear_stat_fit_reg":
+        return generate_auto_linear_stat_fit_data(
+            X, size, epsilon, method="linear_reg", shuffle=shuffle, rescaling=rescaling
+        )
+    elif method == "super_regular_noise":
+        return generate_super_regular_noise_data(
+            X, size, epsilon, shuffle=shuffle, rescaling=rescaling
+        )
+    else:
+        raise ValueError("Unknown method")
